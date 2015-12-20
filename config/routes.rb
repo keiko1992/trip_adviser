a5a9f89b5686b3ba0589470737cb467a77b8ce84
Kazuya Hara <kazuya@9bar.tokyo>
2015-12-21 00:24:14 +0900

    rails g devise Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 # == Route Map
 #
-#     Prefix Verb URI Pattern           Controller#Action
-#       root GET  /                     home#index
-# home_index GET  /home/index(.:format) home#index
+#                Prefix Verb   URI Pattern                     Controller#Action
+#                  root GET    /                               home#index
+#            home_index GET    /home/index(.:format)           home#index
+#     new_admin_session GET    /admins/sign_in(.:format)       admins/sessions#new
+#         admin_session POST   /admins/sign_in(.:format)       admins/sessions#create
+# destroy_admin_session DELETE /admins/sign_out(.:format)      admins/sessions#destroy
+#        admin_password POST   /admins/password(.:format)      admins/passwords#create
+#    new_admin_password GET    /admins/password/new(.:format)  admins/passwords#new
+#   edit_admin_password GET    /admins/password/edit(.:format) admins/passwords#edit
+#                       PATCH  /admins/password(.:format)      admins/passwords#update
+#                       PUT    /admins/password(.:format)      admins/passwords#update
 #
 
 # bundle exec annotate --routes
@@ -10,5 +18,11 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+
+  # Admin
+  devise_for :admins, controllers: {
+    sessions:       "admins/sessions",
+    passwords:      "admins/passwords"
+  }
 
 end
